3001a8cb846b6d4567a1789457a670102bfcdb8e
Barlog <barlog@tanelorn.li>
2017-03-16 15:37:59 +0300

    React dev tools load fix

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Example with WebSocket, STOMP and OAuth2
 
 Authentication with JWT token as query param and custom security filter
 
-###Server
+## Server
 
 * [Spring Boot](https://projects.spring.io/spring-boot/)
 * [Spring Security OAuth](https://projects.spring.io/spring-security-oauth/)
 
-###Client
+## Client
 
 * [React](https://facebook.github.io/react/)
 * [Redux](https://github.com/reactjs/redux)
--- ui/src/index.dev.js
+++ ui/src/index.dev.js
@@ -37,11 +37,12 @@ const middleware = applyMiddleware(
 	authStateMiddleware
 );
 
+const composeEnhancers =
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
 	rootReducer,
-	compose(middleware,
-		window.__REDUX_DEVTOOLS_EXTENSION__ &&
-		window.__REDUX_DEVTOOLS_EXTENSION__())
+	composeEnhancers(middleware)
 );
 
 const history = syncHistoryWithStore(browserHistory, store);
